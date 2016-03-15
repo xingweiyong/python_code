3b3b035f67ab478fef6290cedf308bbd921700b0
xingweiyong <1531694185@qq.com>
2016-03-15 18:03:13 +0800

    Create auto_mail.py

---
+++ auto_mail.py
@@ -0,0 +1,22 @@
+#coding:utf-8
+import smtplib
+import string
+
+HOST='qq.163.com'
+SUBJECT='test email from python'
+TO='1531694185@qq.com'
+FROM='441078355@qq.com'
+text='python rules the all'
+BODY=string.join((
+    'From: %s' %FROM,
+    'To: %s' %TO,
+    'Subject: %s' %SUBJECT,
+    '',
+    text
+    ),'\r\n')
+server=smtplib.SMTP()
+server.connect(HOST,'25')
+server.starttls()
+server.login('441078355@qq.com','bedmayrwrohfbhbe')
+server.sendmail(FROM,TO,BODY)
+server.quit()
