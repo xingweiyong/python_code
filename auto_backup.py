2977b63ea9ae8934ed8233b206a285785c6e2a9c
xingweiyong <1531694185@qq.com>
2016-03-15 14:37:48 +0800

    Update auto_backup.py

--- auto_backup.py
+++ auto_backup.py
@@ -13,22 +13,23 @@ def compareme(dir1,dir2): #递归获取更新项函数
     #将更新文件名或目录追加到holderlist
     [holderlist.append(os.path.abspath(os.path.join(dir1,x))) for x in only_in_one]
     [holderlist.append(os.path.abspath(os.path.join(dir1,x))) for x in diff_in_one]
-    if len(dircomp.common_dirs)>0: #判断是否存在相同子目录，此处只判断相同子目录，因不同子目录会存在于 diff_in_one中
-        for item in dircomp.commom.dirs: #递归子目录
-            compareme(os.path.abs(os.path.join(dir1,item)),\
-                      os.path.abspath(os.oath.join(dir2,item)))
+    if len(dircomp.common_dirs)>0: #判断是否存在相同子目录
+        for item in dircomp.common_dirs: #递归子目录
+            compareme(os.path.abspath(os.path.join(dir1,item)),\
+                      os.path.abspath(os.path.join(dir2,item)))
     return holderlist
 
 def main():
     if len(sys.argv)>2:
         dir1=sys.argv[1]
-        print dir1
+        #print dir1
         dir2=sys.argv[2]
-        print dir2
+        #print dir2
     else:
         print 'Usage:',sys.argv[0],'datadir backupdir'
         sys.exit()
     source_files=compareme(dir1,dir2)
+    #print source_files
     dir1=os.path.abspath(dir1)
 
     if not dir2.endswith('/'): dir2=dir2+'/'
@@ -37,10 +38,10 @@ def main():
     createdir_bool=False
 
     for item in source_files:
-        destination_dir=re.sub(dir1,dir2,item) #将源目录差异文件路径替换成备份路径
-        print destination_dir
+        destination_dir=re.sub('f1','f2',item) #将源目录差异文件路径替换成备份路径，由于转义字符'\'的问题，源与备份内容不能与文件夹f1、f2重名
+        #print destination_dir
         destination_files.append(destination_dir)
-        if os.path.isdir(item): #备份目录中创建 不存在的目录
+        if os.path.isdir(item): #备份目录中创建 不存在的目录,先处理文件夹，之后再用shutil.copy处理文件
             if not os.path.exists(destination_dir):
                 os.makedirs(destination_dir)
                 createdir_bool=True
@@ -49,16 +50,16 @@ def main():
         source_files=[]
         source_files=compareme(dir1,dir2)
         for item in source_files:
-            destination_dir=re.sub(dir1,dir2,item) #将源目录差异文件路径替换成备份路径
-            print destination_dir
+            destination_dir=re.sub('f1','f2',item) #将源目录差异文件路径替换成备份路径
+            #print destination_dir
             destination_files.append(destination_dir)
     print 'update item:'
     print source_files
-    print destination_files
+    #print destination_files
     copy_pair=zip(source_files,destination_files)
     for item in copy_pair:
         if os.path.isfile(item[0]):
-            print item[0],item[1]
+            #print item[0],item[1]
             shutil.copyfile(item[0],item[1])
 if __name__=='__main__':
     main()
