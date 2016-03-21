c3ca7b5a948281e4f8364aae4030c9b0472c2a66
xingweiyong <1531694185@qq.com>
2016-03-21 14:13:21 +0800

    Update scriber4.py

--- scriber4.py
+++ scriber4.py
@@ -4,13 +4,15 @@ from bs4 import BeautifulSoup
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
+#获取字段函数
 def get_sub(str):
     num2=int(str.find('<',2))
     num1=int(str.find('">'))
     return str[num1+2:num2]
+#保存文件函数
 def save_as(str,f):
     f.write(str)
+#检测网络连接异常
 def check_error(web_url):
         try:
             request=urllib2.Request(web_url)
@@ -20,7 +22,7 @@ def check_error(web_url):
         except Exception,e:
             print e
             return False
-
+#主体函数
 def get_web(web_url):
     request=urllib2.Request(web_url)
     response=urllib2.urlopen(request)
@@ -33,21 +35,27 @@ def get_web(web_url):
     for tag in soup.find_all('tr'):
         if tag.has_attr('style'):
             if tag['style']=='height:14.25pt':
+                temp.append(tit)
                 if tag.td.p.span.span!=None:
-                    temp.append(tit)
                     for i in tag.td.p.span.span.stripped_strings:
                         if i !=u'县':
+                            #print i
                             temp.append(i+' ')
-                    if temp[0]!=tit+u'和平区 ':
-                        temp.insert(0,tit+u'和平区 ')
+                else:
+                    for item in tag.td.p.strings:
+                        if item!=u'成交套数（套）':
+                            #print item
+                            temp.append(item+' ')
                 item=tag.select('span[lang="EN-US"]')
                 num_item=len(item)
-                if num_item >11:
+                #print num_item
+                if num_item >6: #过滤杂质
                     for i in range(0,num_item):
                         if item[i].string != None:
-                            temp.append(item[i].string+' ')
+                            if item[i].string !=u'(':
+                                temp.append(item[i].string+' ')
                     temp.append('\r\n')
-            if tag['style']=='height:15.75pt':
+            if tag['style']=='height:15.75pt':#网页样式改变
                 temp.append(tit)
                 if tag.td.p.span.font!=None:
                     #print get_sub(str(tag.td.p.span.font))
@@ -72,12 +80,17 @@ def get_web(web_url):
                         temp.append('\r\n')
     try:
         result_f=file('result3.txt','a')
+        if len(temp)!=0:
+            if temp[1]==tit:
+                del temp[0]
+            if temp[1]==u'(':
+                del temp[0:2]
         for item in temp:
             save_as(item.encode('utf-8'), result_f)
     finally:
         result_f.close()
 if __name__=='__main__':
-    for i in [3010,3014,3074,3052]:#[3,1000,2000,3000,3015]
+    for i in range(2,3076):#[3,1000,2000,3000,3015]
         url='http://www2.tjfdc.gov.cn/Lists/List51/DispForm1.aspx?ID='+ str(i)
         print url
         if check_error(url):
