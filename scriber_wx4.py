86f47d4f8d17dc1ea2bb6c9b0a3550100e9e7d4e
xingweiyong <1531694185@qq.com>
2016-04-13 09:40:06 +0800

    Update scriber_wx4.py

--- scriber_wx4.py
+++ scriber_wx4.py
@@ -10,11 +10,11 @@ import chardet
 result3 = ''#保存规则不能匹配的URL，全局变量需要在不同函数中被调用
 list_failed_url = []
 web_get = '' #全局变量，获取网页源代码
-the_first_round = True
+the_first_round = True #判断是否是第一次下载，第一轮则收集失败的URL进行第二轮下载
 
 class MyFrame(wx.Frame):
     def __init__(self):
-        
+        #工具界面
         wx.Frame.__init__(self,None,-1,'scriber',size = (800,600))
 
         panel = wx.Panel(self)
@@ -26,7 +26,7 @@ class MyFrame(wx.Frame):
 
         label_delay = wx.StaticText(panel,-1,u'延迟:',pos = (640,40),size = (30,27))
         self.text_delay = wx.TextCtrl(panel,-1,pos = (670,37),size = (35,27))
-        label_delay_end = wx.StaticText(panel,-1,'-:',pos = (700,40),size = (15,27))
+        label_delay_end = wx.StaticText(panel,-1,'-',pos = (708,40),size = (15,27))
         self.text_delay_end = wx.TextCtrl(panel,-1,pos = (715,37),size = (35,27))
         
         label_reg = wx.StaticText(panel,-1,'REG1:',pos = (10,100),size = (35,27))
@@ -40,7 +40,7 @@ class MyFrame(wx.Frame):
         button_save = wx.Button(panel,-1,'...',pos = (725,95), size = (25,27))
         button_save.Bind(wx.EVT_LEFT_DOWN,self.SaveAsClick)
 
-        self.text_field = wx.TextCtrl(panel,-1,u'日期',pos = (50,180),size = (140,27))
+        self.text_field = wx.TextCtrl(panel,-1,u'title',pos = (50,180),size = (140,27))
         self.text_reg1 = wx.TextCtrl(panel,-1,u'正则',pos = (240,180),size = (360,27))
         self.listbox_field = wx.ListBox(panel,-1,pos = (50,220),size = (140,110))
         self.listbox_field.Bind(wx.EVT_LISTBOX,self.get_reg)
@@ -65,7 +65,7 @@ class MyFrame(wx.Frame):
     def OnButtonClick_cancle(self,event):
         print 'the button_cancle is clicked'
         sys.exit()
-        
+    #另存为操作响应函数
     def SaveAsClick(self,event):
         wildcard2 = "txt files (*.txt)|*.txt|" \
             "Python source (*.py; *.pyc)|*.py;*.pyc"
@@ -78,7 +78,7 @@ class MyFrame(wx.Frame):
             path = dlg.GetPath()
             self.text_save.Value = path
         dlg.Destroy()
-        
+     #以下为实现增加、修改、删除字段函数   
     def OnClick_add(self,event):
         self.listbox_field.Append(self.text_field.GetValue())
         self.text_field.Clear()
@@ -198,11 +198,12 @@ class MyFrame(wx.Frame):
                     url_next = 'http://club.autohome.com.cn/'+url_temp
 
                     for item in range(0,len(temp_list)):
-                        print 'http://club.autohome.com.cn'+temp_list[item]
-                        self.get_content('http://club.autohome.com.cn'+temp_list[item])
+                        print 'http://club.autohome.com.cn'+temp_list[item][1] + '.html'
+                        self.get_content(temp_list[item][1] + '.html')
+                        #self.get_content('http://club.autohome.com.cn'+temp_list[item])
                         time.sleep(random.randint(int(self.text_delay.GetValue()),int(self.text_delay_end.GetValue())))
                         for i in range(0,num_field):
-                            self.get_regular(list_reg[i],dict_fields[list_field[i]],'http://club.autohome.com.cn'+temp_list[item])
+                            self.get_regular(list_reg[i],dict_fields[list_field[i]],temp_list[item])
                         #time.sleep(random.randint(0,int(self.text_delay.GetValue())))
                         time.sleep(random.randint(int(self.text_delay.GetValue()),int(self.text_delay_end.GetValue())))
                         
