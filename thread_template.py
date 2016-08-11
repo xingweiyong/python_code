d42101e8f51d1a57344dd675739fa46de63914a5
xingweiyong <1531694185@qq.com>
2016-08-11 19:03:01 +0800

    Update thread_template.py

--- thread_template.py
+++ thread_template.py
@@ -2,18 +2,19 @@
 import threading
 from Queue import Queue
 
-myqueue = Queue(1000)
-for i in range(1000):
+myqueue = Queue(100)
+for i in range(100):
     myqueue.put('task%d'%(i+1))
 
 def foo():
     try:
         task = myqueue.get_nowait()
+        with open('thread.txt','a') as f:
+            f.write('%s finished!'%task+'\n')
     except Exception,e:
         pass
     #print '%s execute!'%task
-    with open('thread.txt','a') as f:
-        f.write('%s finished!'%task+'\n')
+    
 
 class myThread(threading.Thread):
     def __init__(self,func):
@@ -22,6 +23,7 @@ class myThread(threading.Thread):
     def run(self):
         while myqueue.qsize() > 0:
             self.func()
+            print self.getName()
 
 def main():
     threads = []
